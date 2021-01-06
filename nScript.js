757c34796d77ecd1c564510aaf78fd469c6b8d93
hwnd-git <tomasz.evyl@gmail.com>
2021-01-06 23:15:11 +0100

    programmable separators for game grid

--- nScript.js
+++ nScript.js
@@ -1,6 +1,10 @@
 import * as utils from './nUtils.js';
 import { solveChainedEquation, normalizeEquation, solveBracketsEquation } from './nEquationSolver.js';
 
+const cellFullSize = utils.getCSSVariable('--cell-size');
+const cellReducedSize = utils.getCSSVariable('--cell-size-reduced');
+const separatorWidth = utils.getCSSVariable('--separator-width');
+
 if (document.readyState == "loading") {
     document.addEventListener('DOMContentLoaded', ready)
 } else {
@@ -24,9 +28,9 @@ function populateTopLegend() {
     const borderWidthString = utils.getCSSVariable('--cell-border');
     let borderWidthNumeric = parseInt(borderWidthString, 10);
 
-    const cellFullSize = utils.getCSSVariable('--cell-size');
-    const cellReducedSize = utils.getCSSVariable('--cell-size-reduced');
-    const separatorWidth = utils.getCSSVariable('--separator-width');
+    // const cellFullSize = utils.getCSSVariable('--cell-size');
+    // const cellReducedSize = utils.getCSSVariable('--cell-size-reduced');
+    // const separatorWidth = utils.getCSSVariable('--separator-width');
     let columnsStyleString = '';
     let rowsStyleString = '';
     let templateAreasString = '';
@@ -72,7 +76,7 @@ function populateTopLegend() {
 
             if (colNo % 5 == 0 && colNo != columnsQty) {
                 let multiplier = colNo / 5;
-                currentArea = currentArea.concat(`s${multiplier} `);
+                currentArea = currentArea.concat(`sv${multiplier} `);
             }
 
             templateAreasString = templateAreasString.concat(currentArea);
@@ -105,9 +109,9 @@ function populateSideLegend() {
     const borderWidthString = utils.getCSSVariable('--cell-border');
     let borderWidthNumeric = parseInt(borderWidthString, 10);
 
-    const cellFullSize = utils.getCSSVariable('--cell-size');
-    const cellReducedSize = utils.getCSSVariable('--cell-size-reduced');
-    const separatorWidth = utils.getCSSVariable('--separator-width');
+    // const cellFullSize = utils.getCSSVariable('--cell-size');
+    // const cellReducedSize = utils.getCSSVariable('--cell-size-reduced');
+    // const separatorWidth = utils.getCSSVariable('--separator-width');
     let rowsStyleString = '';
     let columnsStyleString = '';
     let templateAreasString = '';
@@ -157,16 +161,16 @@ function populateSideLegend() {
             let multiplier = rowNo / 5;
             for (let colNo = gridWidth; colNo >= 1; colNo--) {
                 if (colNo == 1) {
-                    currentArea = currentArea.concat(`s${multiplier}" `);
+                    currentArea = currentArea.concat(`sh${multiplier}" `);
                 } else {
-                    currentArea = currentArea.concat(`s${multiplier} `);
+                    currentArea = currentArea.concat(`sh${multiplier} `);
                 }
             }
             templateAreasString = templateAreasString.concat(currentArea);
         }
     }
 
-    console.log(templateAreasString);
+    //console.log(templateAreasString);
 
     const legendElement = document.getElementById('legend-vertical')
     legendElement.style.gridTemplateColumns = columnsStyleString;
@@ -190,16 +194,100 @@ function populateSideLegend() {
     }
 }
 
-
-
-
 function populateGameGrid() {
     const width = utils.getCSSVariable('--stage-h-size');
     const height = utils.getCSSVariable('--stage-v-size');
     const borderWidthString = utils.getCSSVariable('--cell-border');
     let borderWidthNumeric = parseInt(borderWidthString, 10);
 
+    // const cellFullSize = utils.getCSSVariable('--cell-size');
+    // const cellReducedSize = utils.getCSSVariable('--cell-size-reduced');
+    // const separatorWidth = utils.getCSSVariable('--separator-width');
+    let columnsStyleString = '';
+    let rowsStyleString = '';
+    let templateAreasString = '';
+
+    //columns template
+    for (let colNo = 1; colNo <= width; colNo++) {
+
+        let currentColumnWidth = ''
+
+        if (colNo == width) {
+            currentColumnWidth = `${cellFullSize}`;
+        } else {
+            currentColumnWidth = `${cellReducedSize} `;
+        }
+
+        if (colNo % 5 == 0 && colNo != width) {
+            currentColumnWidth = currentColumnWidth.concat(`${separatorWidth} `);
+        }
+
+        columnsStyleString = columnsStyleString.concat(currentColumnWidth);
+    }
+
+    console.log('columns: ', columnsStyleString);
+
+    //rows template
+    for (let rowNo = 1; rowNo <= height; rowNo++) {
+
+        let currentRowWidth = ''
+
+        if (rowNo == height) {
+            currentRowWidth = `${cellFullSize}`;
+        } else {
+            currentRowWidth = `${cellReducedSize} `;
+        }
+
+        if (rowNo % 5 == 0 && rowNo != height) {
+            currentRowWidth = currentRowWidth.concat(`${separatorWidth} `);
+        }
+        rowsStyleString = rowsStyleString.concat(currentRowWidth);
+    }
+
+    console.log('rows: ', rowsStyleString);
+
+    //grid areas
+    for (let rowNo = 1; rowNo <= height; rowNo++) {
+        templateAreasString = templateAreasString.concat('"');
+        for (let colNo = 1; colNo <= width; colNo++) {
+            let currentArea = '';
+
+            if (colNo == width) {
+                currentArea = `a${colNo}-${rowNo}" `;
+            } else {
+                currentArea = `a${colNo}-${rowNo} `;
+            }
+
+            if (colNo % 5 == 0 && colNo != width) {
+                let segmentsCounter = Math.floor((parseInt(rowNo) - 1) / 5) + 1;
+                let multiplier = colNo / 5;
+                currentArea = currentArea.concat(`sv${multiplier}-${segmentsCounter} `);
+            }
+
+            templateAreasString = templateAreasString.concat(currentArea);
+        }
+
+        if (rowNo % 5 == 0 && rowNo != height) {
+            let currentArea = '"';
+            let multiplier = rowNo / 5;
+            let widthWithSeparators = parseInt(width) + Math.floor((width - 1) / 5);
+            for (let colNo = 1; colNo <= widthWithSeparators; colNo++) {
+                if (colNo == widthWithSeparators) {
+                    currentArea = currentArea.concat(`sh${multiplier}" `);
+                } else {
+                    currentArea = currentArea.concat(`sh${multiplier} `);
+                }
+            }
+            templateAreasString = templateAreasString.concat(currentArea);
+        }
+    }
+
+    console.log('template: ', templateAreasString);
+
     const legendElement = document.getElementById('game-grid')
+    legendElement.style.gridTemplateColumns = columnsStyleString;
+    legendElement.style.gridTemplateRows = rowsStyleString;
+    legendElement.style.gridTemplateAreas = templateAreasString;
 
     for (let colNo = 1; colNo <= width; colNo++) {
         for (let rowNo = 1; rowNo <= height; rowNo++) {
@@ -207,8 +295,10 @@ function populateGameGrid() {
             //let no = height - rowNo + 1;
             cell.className = 'cell cell-game';
             cell.id = `cell${colNo}-${rowNo}`;
-            cell.style.gridColumnStart = `${colNo}`;
-            cell.style.gridRowStart = `${rowNo}`;
+            // cell.style.gridColumnStart = `${colNo}`;
+            // cell.style.gridRowStart = `${rowNo}`;
+
+            cell.style.gridArea = `a${colNo}-${rowNo}`;
 
             //translateCellToRemoveDoubleBorder(cell, borderWidthNumeric);
 
